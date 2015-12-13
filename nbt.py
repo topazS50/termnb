f96c88900d60e160c83967900410b8d53ff4cb9c
Kenji Ogawa <ogawaknj@gmail.com>
2015-12-13 16:16:36 +0900

    update

--- nbt.py
+++ nbt.py
@@ -7,67 +7,84 @@ import subprocess
 import itertools
 
 
-LENGTH = 50
-HEAD_EXCEPTION = '=====Exception=' + LENGTH * '='
-TAIL_EXCEPTION = '-----Exception-' + LENGTH * '-'
-HEAD_CODE = '===============' + LENGTH * '='
-TAIL_CODE = '---------------' + LENGTH * '-'
-HEAD_MD = '=====Markdown==' + LENGTH * '='
-TAIL_MD = '-----Markdown--' + LENGTH * '-'
-HEAD_EXEC = '=====Out=======' + LENGTH * '='
-TAIL_EXEC = '---------------' + LENGTH * '-'
+LENGTH_BARS = 50
+HEAD_CODE = '===============' + LENGTH_BARS * '='
+TAIL_CODE = '---------------' + LENGTH_BARS * '-'
+HEAD_MD = '=====Markdown==' + LENGTH_BARS * '='
+TAIL_MD = '-----Markdown--' + LENGTH_BARS * '-'
+HEAD_EXEC = '=====Out=======' + LENGTH_BARS * '='
+TAIL_EXEC = '---------------' + LENGTH_BARS * '-'
+HEAD_EXCEPTION = '=====Exception=' + LENGTH_BARS * '='
+TAIL_EXCEPTION = '-----Exception-' + LENGTH_BARS * '-'
+EDITOR = 'pyvim'
+FILETMP = '__tmp.py'
 
 
 count_ = itertools.count()
 MODE_SHOWCELL = count_.next()
-MODE0 = count_.next()
-MODE1 = count_.next()
+MODE_INPUT0 = count_.next()
+MODE_INPUT1 = count_.next()
+MODE_INPUT2 = count_.next()
 MODE_EXECCELL = count_.next()
 MODE_EDIT = count_.next()
 MODE_COMMANDPROMPT = count_.next()
+MODE_INSERT = count_.next()
+MODE_DELETE = count_.next()
+MODE_LIST = count_.next()
+MODE_EXIT = count_.next()
 
 
-def get_user_input():
-    uinput = raw_input('termnb>>')
-    return uinput
+def get_sourcestring(cell):
+    if cell['source'] == []:
+        return ''
+    elif len(cell['source']) == 1:
+        return cell['source'][0]
+    else:
+        return reduce(lambda a, b: a + b, cell['source'])
+
+
+def string_to_cell(source_string):
+    cell_source = [eachrow + '\n' for eachrow in source_string.split('\n')]
+    cell_source[-1].rstrip('\n')
+    cell_source[-1].rstrip('\n')
+    return cell_source
 
 
 def show_cell(cell, id_current_cell):
     tag_id = ': ' + str(id_current_cell)
-    if cell['source'] != []:
-        source_string = reduce(lambda a, b: a + b, cell['source'])
-        if cell['cell_type'] == 'code':
-            print HEAD_CODE + tag_id
-            print source_string.rstrip('\n')
-            print TAIL_CODE + tag_id
-        else:
-            print HEAD_MD + tag_id
-            print source_string.rstrip('\n')
-            print TAIL_MD + tag_id
+    source_string = get_sourcestring(cell)
+    if cell['cell_type'] == 'code':
+        print HEAD_CODE + tag_id
+        print source_string.rstrip('\n')
+        print TAIL_CODE + tag_id
+    else:
+        print HEAD_MD + tag_id
+        print source_string.rstrip('\n')
+        print TAIL_MD + tag_id
 
 
-def show_all(js_ipynb):
-    for cell in js_ipynb['cells']:
-        show_cell(cell)
+def print_exception(info):
+    print HEAD_EXCEPTION
+    print info[0]
+    print info[1]
+    print info[2]
+    print TAIL_EXCEPTION
 
 
-def get_sourcestring(cell):
-    return reduce(lambda a, b: a + b, cell['source'])
-
-
-def controller(cells):
+def parse_cells(cells):
     id_current_cell = 0
     mode = MODE_SHOWCELL
-    while id_current_cell < len(cells):
+    while (id_current_cell < len(cells)) and (-1 < id_current_cell) and (mode != MODE_EXIT):
         current_cell = cells[id_current_cell]
         if mode == MODE_SHOWCELL:
             show_cell(current_cell, id_current_cell)
             if (current_cell['cell_type'] == 'code') and (current_cell['source'] != ''):
-                mode = MODE0
+                mode = MODE_INPUT0
             else:
-                mode = MODE1
-        elif mode == MODE0:
-            input_ = raw_input('execute?[Y-es,n-ext,b-ack,e-dit,c-ommand]')
+                mode = MODE_INPUT1
+
+        elif mode == MODE_INPUT0:
+            input_ = raw_input('execute?[Y-es,n-ext,b-ack,e-dit,c-ommand,i-nsert,d-elete,l-ist,eXit]')
             if input_ in ['', 'y', 'Y']:
                 mode = MODE_EXECCELL
             elif input_ in ['n']:
@@ -80,9 +97,38 @@ def controller(cells):
                 mode = MODE_EDIT
             elif input_ in ['c']:
                 mode = MODE_COMMANDPROMPT
+            elif input_ in ['i']:
+                mode = MODE_INSERT
+            elif input_ in ['d']:
+                mode = MODE_DELETE
+            elif input_ in ['l']:
+                mode = MODE_LIST
+            elif input_ in ['x','X','exit']:
+                mode = MODE_EXIT
 
-        elif mode == MODE1:
-            input_ = raw_input('[N-ext,b-ack,e-dit,c-ommand]')
+        elif mode == MODE_INPUT1:
+            input_ = raw_input('[N-ext,b-ack,e-dit,c-ommand,i-nsert,d-elete,l-ist,eXit]')
+            if input_ in ['', 'n','N']:
+                id_current_cell += 1
+                mode = MODE_SHOWCELL
+            elif input_ in ['b','back']:
+                id_current_cell -= 1
+                mode = MODE_SHOWCELL
+            elif input_ in ['e','edit']:
+                mode = MODE_EDIT
+            elif input_ in ['c','command']:
+                mode = MODE_COMMANDPROMPT
+            elif input_ in ['i','insert']:
+                mode = MODE_INSERT
+            elif input_ in ['d','delte']:
+                mode = MODE_DELETE
+            elif input_ in ['l','list']:
+                mode = MODE_LIST
+            elif input_ in ['x','X','exit']:
+                mode = MODE_EXIT
+
+        elif mode == MODE_INPUT2:
+            input_ = raw_input('[N-ext,b-ack,e-dit,c-ommand,p-rint]')
             if input_ in ['', 'n']:
                 id_current_cell += 1
                 mode = MODE_SHOWCELL
@@ -93,25 +139,22 @@ def controller(cells):
                 mode = MODE_EDIT
             elif input_ in ['c']:
                 mode = MODE_COMMANDPROMPT
+            elif input_ in ['p']:
+                exec('print ' + current_cell['source'][-1])
 
         elif mode == MODE_EXECCELL:
             source_string = get_sourcestring(current_cell)
             try:
-                print HEAD_EXEC
                 success_command = False
+                print HEAD_EXEC
                 exec(source_string)
-                success_command = True
                 print TAIL_EXEC
+                success_command = True
             except:
-                print HEAD_EXCEPTION
-                print sys.exc_info()[0]
-                print sys.exc_info()[1]
-                print sys.exc_info()[2]
-                print TAIL_EXCEPTION
-                mode = MODE1
-            if success_command:
-                id_current_cell += 1
+                print_exception(sys.exc_info())
                 mode = MODE_SHOWCELL
+            if success_command:
+                mode = MODE_INPUT2
 
         elif mode == MODE_COMMANDPROMPT:
             command_ = raw_input(':')
@@ -121,50 +164,55 @@ def controller(cells):
                     exec(command_)
                     success_command = True
                 except:
-                    print HEAD_EXCEPTION
-                    print sys.exc_info()[0]
-                    print sys.exc_info()[1]
-                    print sys.exc_info()[2]
-                    print TAIL_EXCEPTION
+                    print_exception(sys.exc_info())
                 if success_command:
                     try:
                         exec('print ' + command_)
                     except:
                         pass
+                command_ = raw_input(':')
             mode = MODE_SHOWCELL
+
         elif mode == MODE_EDIT:
             source_string = get_sourcestring(current_cell)
-            with open('tmp.py', 'w') as fw:
+            with open(FILETMP, 'w') as fw:
                 fw.write(source_string.encode('utf-8'))
-            subprocess.check_call('vim ./tmp.py', shell=True)
-            with open('tmp.py', 'r') as fr:
+            subprocess.check_call(EDITOR + ' ' + FILETMP, shell=True)
+            with open(FILETMP, 'r') as fr:
                 source_string = fr.read().decode('utf-8').rstrip('\n')
-                current_cell['source'] = [eachrow + '\n' for eachrow in source_string.split('\n')]
-                current_cell['source'][-1].rstrip('\n')
-                current_cell['source'][-1].rstrip('\n')
+                current_cell['source'] = string_to_cell(source_string)
             mode = MODE_SHOWCELL
 
+        elif mode == MODE_INSERT:
+            cells_new0 = cells[:id_current_cell]
+            cell_insert = {'cell_type': 'code', 'execution_count': None, 'metadata': {'collapsed': False}, 'outputs': [], 'source': []}
+            cells_new1 = cells[id_current_cell:]
+            cells = cells_new0 + [cell_insert] + cells_new1
+            id_current_cell += 1
+            mode = MODE_EDIT
 
-def exec_cell(cell):
-    if cell['cell_type'] == 'code':
-        source_string = reduce(lambda a, b: a + b, cell['source'])
-        exec(source_string)
-        # for line in cell['source']:
-        #    exec(line)
-    else:
-        pass
-        # show_cell(cell)
+        elif mode == MODE_DELETE:
+            input_ = raw_input('Do you reall want to delete this cell?[N-o,yes]')
+            if input_ == 'yes':
+                cells_new0 = cells[:id_current_cell]
+                cells_new1 = cells[id_current_cell+1:]
+                cells = cells_new0 + cells_new1
+            else:
+                pass
+            mode = MODE_SHOWCELL
 
+        elif mode == MODE_LIST:
+            for i, cell in enumerate(cells):
+                show_cell(cell, i)
+            mode = MODE_SHOWCELL
 
-def exec_nb(js_ipynb):
-    for cell in js_ipynb['cells']:
-        exec_cell(cell)
+    return cells
 
 
 def load_ipynb(filename):
-    f = open(filename)
-    js_ipynb = json.load(f)
-    return js_ipynb['cells']
+    with open(filename) as f:
+        js_ipynb = json.load(f)
+        return js_ipynb
 
 
 def main():
@@ -173,12 +221,13 @@ def main():
         quit()
 
     filename = str(sys.argv[1])
-    f = open(filename)
-    js_ipynb = json.load(f)
+    ipynb = load_ipynb(filename)
+    cells = ipynb['cells']
 
-    #show_all(js_ipynb)
-    cells = js_ipynb['cells']
-    controller(cells)
+    ipynb['cells'] = parse_cells(cells)
+
+    with open(filename, 'w') as fw:
+        json.dump(ipynb, fw)
 
 if __name__ == '__main__':
     main()
